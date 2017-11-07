12cfd7b0677773e561370fa72081146f94047e30
Alex Benoit <alex@lewagon.com>
2017-11-07 14:39:42 +0000

    added picture url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106231055) do
+ActiveRecord::Schema.define(version: 20171107143847) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "exercise_results", force: :cascade do |t|
+    t.string "path"
+    t.text "output"
+    t.integer "tests", default: 0
+    t.integer "failures", default: 0
+    t.boolean "syntax_error", default: false
+    t.boolean "rake_aborted", default: false
+    t.integer "offenses", default: 0
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "code"
+    t.index ["user_id"], name: "index_exercise_results_on_user_id"
+  end
 
   create_table "flats", force: :cascade do |t|
     t.string "name"
@@ -18,6 +36,19 @@ ActiveRecord::Schema.define(version: 20171106231055) do
     t.integer "price_per_night"
     t.integer "number_of_guests"
     t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "picture_url"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "name"
+    t.string "email"
+    t.string "provider"
+    t.string "token"
+    t.string "uid"
+    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
